5d80a716731ce0e711f8ec0e900f8d91847ae478
Kishan Patel <kpatel16@tamu.edu>
2020-07-08 22:47:49 -0500

    Split TitleBar from Home

--- parcel-service/src/routes/Home/index.jsx
+++ parcel-service/src/routes/Home/index.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Section, Container, Title, Block } from "rbx";
 
+import BookmarksGrid from './components/BookmarksGrid';
+
 import static from './static.yaml';
+
+const TitleBar = () => (
+  <Block textAlign="centered">
+    <Title>{static.appTitle}</Title>
+    <Title subtitle>{static.appSubtitle}</Title>
+  </Block>
+);
 
 const Home = () => {
   return (
     <Section>
       <Container>
-        <Block textAlign="centered">
-          <Title>{static.appTitle}</Title>
-          <Title subtitle>{static.appSubtitle}</Title>
-        </Block>
+        <TitleBar />
+        <BookmarksGrid />
       </Container>
     </Section>
   );
