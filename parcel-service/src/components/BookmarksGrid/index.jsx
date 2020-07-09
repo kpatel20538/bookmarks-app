b087b0414d6c5cf02e61e3d48bf5dec851ab9025
Kishan Patel <kpatel16@tamu.edu>
2020-07-09 02:40:59 -0500

    Implement flip grid

--- parcel-service/src/components/BookmarksGrid/index.jsx
+++ parcel-service/src/components/BookmarksGrid/index.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { Flipped, Flipper, spring } from "react-flip-toolkit";
+import { Column } from "rbx";
+
+import BookmarkCard from "/components/BookmarkCard";
+
+const onAppear = (el, index) => {
+  spring({
+    config: { overshootClamping: true },
+    onUpdate: (val) => {
+      el.style.transform = `scaleX(${val})`;
+      el.style.opacity = val;
+    },
+    delay: index * 50,
+  });
+};
+
+const onExit = (el, index, removeElement) => {
+  spring({
+    config: { overshootClamping: true },
+    onUpdate: (val) => {
+      el.style.opacity = 1 - val;
+      el.style.transform = `scaleX(${1 - val})`;
+    },
+    delay: index * 50,
+    onComplete: removeElement,
+  });
+
+  return () => {
+    removeElement();
+  };
+};
+
+const List = () => {
+  const data = [];
+  return (
+    <Flipper flipKey={JSON.stringify(data)}>
+      <Column.Group multiline>
+        {data.map((bookmark) => (
+          <Flipped
+            key={bookmark.id}
+            flipId={bookmark.id}
+            onAppear={onAppear}
+            onExit={onExit}
+          >
+            <Column
+              mobile={{ size: "full" }}
+              tablet={{ size: "half" }}
+              desktop={{ size: "one-quarter" }}
+            >
+              <BookmarkCard bookmark={bookmark} />
+            </Column>
+          </Flipped>
+        ))}
+      </Column.Group>
+    </Flipper>
+  );
+};
+
+// ---
 
 const BookmarksGrid = () => {
-  return null;
+  return <List />;
 };
 
 export default BookmarksGrid;
