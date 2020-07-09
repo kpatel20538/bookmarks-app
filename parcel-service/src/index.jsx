a1a3e4bfed4da2c8329589983c897fae95defc27
Kishan Patel <kpatel16@tamu.edu>
2020-07-08 23:03:20 -0500

    Adjusted module resolution and added Suspense

--- parcel-service/src/index.jsx
+++ parcel-service/src/index.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { render } from "react-dom";
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import { client } from "./helpers/apollo";
+import { Loader } from "rbx";
+import { client } from "/helpers/apollo";
 import "rbx/index.css";
+
+const Home = React.lazy(() => import('/routes/Home'));
 
 const Root = () => (
   <ApolloProvider client={client}>
     <Router>
       <Switch>
         <Route path="/">
-          <Home />
+          <Suspense fallback={<Loader />}>
+            <Home />
+          </Suspense>
         </Route>
       </Switch>
     </Router>
--- parcel-service/src/routes/Home/index.jsx
+++ parcel-service/src/routes/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Section, Container, Title, Block } from "rbx";
 
-import BookmarksGrid from './components/BookmarksGrid';
+import BookmarksGrid from "/components/BookmarksGrid";
 
 import static from './static.yaml';
 
